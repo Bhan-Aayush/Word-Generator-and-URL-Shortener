6875d59506633517c1a76fe37727f42895251689
Plasmatic <63007327+Bhan-Aayush@users.noreply.github.com>
2021-08-13 18:27:31 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,70 @@
+const dataMuseUrl = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_jjb=';
+
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+const getSuggestions = async () => {
+  const wordQuery = inputField.value;
+  const endpoint = dataMuseUrl + queryParams + wordQuery;
+
+  try{
+    const response =  await fetch(endpoint);
+    if(response.ok){
+      let jsonResponse = await response.json();
+			renderWordResponse(jsonResponse);
+    }
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  getSuggestions();
+}
+
+submit.addEventListener('click', displaySuggestions);
+
+const apiKey = '<Your API Key>';
+const rebrandlyEndpoint = 'https://api.rebrandly.com/v1/links';
+
+const shortenButton = document.querySelector('#shorten');
+
+const shortenUrl = async () =>{
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+
+  try{
+    const response =  await fetch(rebrandlyEndpoint, {
+      method: 'POST',
+      body: data,
+      headers: {
+        "Content-type": "application/json",
+        'apikey': apiKey
+      }
+    })
+    if(response.ok){
+      const jsonResponse = await response.json();
+			renderByteResponse(jsonResponse);
+    }
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
